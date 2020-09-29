dd201a78f5fa9fa93acdad908ff0c9bac4f851c2
Vishvajit Kher <vishvajit@findnemo.in>
2020-09-28 20:15:33 -0400

    Minor update

--- lib/api/device_api.dart
+++ lib/api/device_api.dart
@@ -54,12 +54,70 @@ class DeviceApi {
   /// Returns number of device deleted
   ///
   /// Delete a device for a user by id
-  Future<DeviceResponse> deviceIdDelete(int id) async {
+  Future<DeleteRecordsResponse> deviceIdDelete(int id) async {
     Response response = await deviceIdDeleteWithHttpInfo(id);
     if(response.statusCode >= 400) {
       throw ApiException(response.statusCode, _decodeBodyBytes(response));
     } else if(response.body != null) {
-      return apiClient.deserialize(_decodeBodyBytes(response), 'DeviceResponse') as DeviceResponse;
+      return apiClient.deserialize(_decodeBodyBytes(response), 'DeleteRecordsResponse') as DeleteRecordsResponse;
+    } else {
+      return null;
+    }
+  }
+
+  /// Returns number of device deleted with HTTP info returned
+  ///
+  /// Delete a device token for a user by id
+  Future<Response> deviceIdFcmTokenDeleteWithHttpInfo(int id) async {
+    Object postBody;
+
+    // verify required params are set
+    if(id == null) {
+     throw ApiException(400, "Missing required param: id");
+    }
+
+    // create path and map variables
+    String path = "/device/{id}/fcm-token/".replaceAll("{format}","json").replaceAll("{" + "id" + "}", id.toString());
+
+    // query params
+    List<QueryParam> queryParams = [];
+    Map<String, String> headerParams = {};
+    Map<String, String> formParams = {};
+
+    List<String> contentTypes = [];
+
+    String nullableContentType = contentTypes.isNotEmpty ? contentTypes[0] : null;
+    List<String> authNames = ["BearerAuth"];
+
+    if(nullableContentType != null && nullableContentType.startsWith("multipart/form-data")) {
+      bool hasFields = false;
+      MultipartRequest mp = MultipartRequest(null, null);
+      if(hasFields)
+        postBody = mp;
+    }
+    else {
+    }
+
+    var response = await apiClient.invokeAPI(path,
+                                             'DELETE',
+                                             queryParams,
+                                             postBody,
+                                             headerParams,
+                                             formParams,
+                                             nullableContentType,
+                                             authNames);
+    return response;
+  }
+
+  /// Returns number of device deleted
+  ///
+  /// Delete a device token for a user by id
+  Future<UpdateRecordsResponse> deviceIdFcmTokenDelete(int id) async {
+    Response response = await deviceIdFcmTokenDeleteWithHttpInfo(id);
+    if(response.statusCode >= 400) {
+      throw ApiException(response.statusCode, _decodeBodyBytes(response));
+    } else if(response.body != null) {
+      return apiClient.deserialize(_decodeBodyBytes(response), 'UpdateRecordsResponse') as UpdateRecordsResponse;
     } else {
       return null;
     }
--- lib/model/device_with_user_response.dart
+++ lib/model/device_with_user_response.dart
@@ -14,6 +14,8 @@ class DeviceWithUserResponse {
   
   String fcmToken = null;
   
+  String locale = null;
+  
   UserResponse user = null;
   
   String updatedAt = null;
@@ -23,7 +25,7 @@ class DeviceWithUserResponse {
 
   @override
   String toString() {
-    return 'DeviceWithUserResponse[id=$id, uId=$uId, model=$model, make=$make, userId=$userId, fcmToken=$fcmToken, user=$user, updatedAt=$updatedAt, createdAt=$createdAt, ]';
+    return 'DeviceWithUserResponse[id=$id, uId=$uId, model=$model, make=$make, userId=$userId, fcmToken=$fcmToken, locale=$locale, user=$user, updatedAt=$updatedAt, createdAt=$createdAt, ]';
   }
 
   DeviceWithUserResponse.fromJson(Map<String, dynamic> json) {
@@ -34,6 +36,7 @@ class DeviceWithUserResponse {
     make = json['make'];
     userId = json['userId'];
     fcmToken = json['fcmToken'];
+    locale = json['locale'];
     user = (json['user'] == null) ?
       null :
       UserResponse.fromJson(json['user']);
@@ -55,6 +58,8 @@ class DeviceWithUserResponse {
       json['userId'] = userId;
     if (fcmToken != null)
       json['fcmToken'] = fcmToken;
+    if (locale != null)
+      json['locale'] = locale;
     if (user != null)
       json['user'] = user;
     if (updatedAt != null)
