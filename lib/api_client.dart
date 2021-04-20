bfb471c208fb1bafb2a08d76a346c73f0f6c6a0d
Vishvajit Kher <vishvajit.kher@georgebrown.ca>
2021-04-19 21:45:05 -0400

    Minor update

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -8,8 +8,8 @@ class QueryParam {
 }
 
 class ApiClient {
-
   String basePath;
+  String accessToken;
   var client = Client();
 
   Map<String, String> _defaultHeaderMap = {};
@@ -18,13 +18,18 @@ class ApiClient {
   final _regList = RegExp(r'^List<(.*)>$');
   final _regMap = RegExp(r'^Map<String,(.*)>$');
 
-  ApiClient({this.basePath = "https://localhost"}) {
+  ApiClient({
+    this.accessToken,
+    this.basePath = "https://production.user-api.api.findnemo.in",
+  }) {
     // Setup authentications (key: authentication name, value: authentication).
-    _authentications['BearerAuth'] = HttpBearerAuth();
+    if (this.accessToken != null && this.accessToken is String) {
+      _authentications['BearerAuth'] = OAuth(accessToken: this.accessToken);
+    }
   }
 
   void addDefaultHeader(String key, String value) {
-     _defaultHeaderMap[key] = value;
+    _defaultHeaderMap[key] = value;
   }
 
   dynamic _deserialize(dynamic value, String targetType) {
@@ -150,9 +155,11 @@ class ApiClient {
           }
       }
     } on Exception catch (e, stack) {
-      throw ApiException.withInner(500, 'Exception during deserialization.', e, stack);
+      throw ApiException.withInner(
+          500, 'Exception during deserialization.', e, stack);
     }
-    throw ApiException(500, 'Could not find a suitable class for deserialization');
+    throw ApiException(
+        500, 'Could not find a suitable class for deserialization');
   }
 
   dynamic deserialize(String json, String targetType) {
@@ -177,24 +184,22 @@ class ApiClient {
 
   // We don't use a Map<String, String> for queryParams.
   // If collectionFormat is 'multi' a key might appear multiple times.
-  Future<Response> invokeAPI(String path,
-                             String method,
-                             Iterable<QueryParam> queryParams,
-                             Object body,
-                             Map<String, String> headerParams,
-                             Map<String, String> formParams,
-                             String nullableContentType,
-                             List<String> authNames) async {
-
+  Future<Response> invokeAPI(
+      String path,
+      String method,
+      Iterable<QueryParam> queryParams,
+      Object body,
+      Map<String, String> headerParams,
+      Map<String, String> formParams,
+      String nullableContentType,
+      List<String> authNames) async {
     _updateParamsForAuth(authNames, queryParams, headerParams);
 
     var ps = queryParams
-      .where((p) => p.value != null)
-      .map((p) => '${p.name}=${Uri.encodeQueryComponent(p.value)}');
+        .where((p) => p.value != null)
+        .map((p) => '${p.name}=${Uri.encodeQueryComponent(p.value)}');
 
-    String queryString = ps.isNotEmpty ?
-                         '?' + ps.join('&') :
-                         '';
+    String queryString = ps.isNotEmpty ? '?' + ps.join('&') : '';
 
     String url = basePath + path + queryString;
 
@@ -204,7 +209,7 @@ class ApiClient {
       headerParams['Content-Type'] = contentType;
     }
 
-    if(body is MultipartRequest) {
+    if (body is MultipartRequest) {
       var request = MultipartRequest(method, Uri.parse(url));
       request.fields.addAll(body.fields);
       request.files.addAll(body.files);
@@ -213,9 +218,11 @@ class ApiClient {
       var response = await client.send(request);
       return Response.fromStream(response);
     } else {
-      var msgBody = nullableContentType == "application/x-www-form-urlencoded" ? formParams : serialize(body);
-      final nullableHeaderParams = (headerParams.isEmpty)? null: headerParams;
-      switch(method) {
+      var msgBody = nullableContentType == "application/x-www-form-urlencoded"
+          ? formParams
+          : serialize(body);
+      final nullableHeaderParams = (headerParams.isEmpty) ? null : headerParams;
+      switch (method) {
         case "POST":
           return client.post(url, headers: nullableHeaderParams, body: msgBody);
         case "PUT":
@@ -223,7 +230,8 @@ class ApiClient {
         case "DELETE":
           return client.delete(url, headers: nullableHeaderParams);
         case "PATCH":
-          return client.patch(url, headers: nullableHeaderParams, body: msgBody);
+          return client.patch(url,
+              headers: nullableHeaderParams, body: msgBody);
         case "HEAD":
           return client.head(url, headers: nullableHeaderParams);
         default:
@@ -234,10 +242,12 @@ class ApiClient {
 
   /// Update query and header parameters based on authentication settings.
   /// @param authNames The authentications to apply
-  void _updateParamsForAuth(List<String> authNames, List<QueryParam> queryParams, Map<String, String> headerParams) {
+  void _updateParamsForAuth(List<String> authNames,
+      List<QueryParam> queryParams, Map<String, String> headerParams) {
     authNames.forEach((authName) {
       Authentication auth = _authentications[authName];
-      if (auth == null) throw ArgumentError("Authentication undefined: " + authName);
+      if (auth == null)
+        throw ArgumentError("Authentication undefined: " + authName);
       auth.applyToParams(queryParams, headerParams);
     });
   }
